0a6e2a8d8466bf6123b1c4d65961520992abd03e
Subha Sasmal <subha.sasmal@RFPL.COM>
2026-04-13 23:52:23 +0530

    build changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export',   // This generates a static 'out' folder
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
