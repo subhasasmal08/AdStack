ddd49bb44c440b39058c318bc8fba3e9baa4f476
subhasasmal08 <subhasasmal08@gmail.com>
2026-07-18 02:21:58 +0530

    build fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',   // This generates a static 'out' folder
+  output: 'export',   // This generates a static 'out' folder
   images: {
     unoptimized: true, // Required for static export
   },
